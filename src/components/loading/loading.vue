15613475d5f7de05fe42d1470a71dfd6fb97aa12
wsgfsm <1552660178@qq.com>
2020-03-05 17:08:45 +0800

    3.5智障1.0

---
+++ src/components/loading/loading.vue
@@ -0,0 +1,98 @@
+<style lang="scss" scoped>
+.loading {
+  position: absolute;
+  z-index: 100;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  
+}
+.style1 {
+  color: #000;
+  font-size: 60px;
+  line-height: 100vh;
+  background-color: rgba(1, 1, 1, 0.6);
+}
+.style2 {
+  color: #f40;
+  font-size: 30px;
+  line-height: 71vh;
+  background-color: #444;
+}
+.title {
+  text-align: center;
+  font-family: 微软雅黑;
+  font-weight: 900;
+  letter-spacing: 0.1em;
+}
+.letter {
+  animation: bounce 0.75s cubic-bezier(0.05, 0, 0.2, 1) infinite alternate;
+  display: inline-block;
+  
+}
+.letter:nth-child(1) {
+  animation-delay: 0s;
+}
+.letter:nth-child(2) {
+  animation-delay: 0.08s;
+}
+.letter:nth-child(3) {
+  animation-delay: 0.16s;
+}
+.letter:nth-child(4) {
+  animation-delay: 0.24s;
+}
+.letter:nth-child(5) {
+  animation-delay: 0.35s;
+}
+.letter:nth-child(6) {
+  animation-delay: 0.43s;
+}
+.letter:nth-child(7) {
+  animation-delay: 0.52s;
+}
+.letter:nth-child(8) {
+  animation-delay: 0.6s;
+}
+
+@keyframes bounce {
+  0% {
+    transform: translate3d(0, 0, 0);
+    text-shadow: rgba(255, 255, 255, 0.4) 0 0 0.05em;
+  }
+  100% {
+    transform: translate3d(0, -0.6em, 0);
+    text-shadow: rgba(255, 255, 255, 0.4) 0 1em 0.35em;
+  }
+}
+</style>
+<template>
+  <div class="loading" :class="{style1:type==1,style2:type==2}">
+    <div class="title">
+      <span class="letter">L</span>
+      <span class="letter">o</span>
+      <span class="letter">a</span>
+      <span class="letter">d</span>
+      <span class="letter">i</span>
+      <span class="letter">n</span>
+      <span class="letter">g</span>
+      <span class="letter">…</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component({
+  props: {
+    type: Number,
+  }
+})
+export default class login extends Vue {
+  mounted() {}
+}
+</script>
+
--- src/views/index.vue
+++ src/views/index.vue
@@ -19,7 +19,7 @@
         height: 13vh;
         width: 13vh;
         border-radius: 50%;
-        background-color: #666;
+        background-color: #fff;
         background-image: url(../assets/blind.png);
         background-position: 70% center;
         background-size: 66%;
@@ -29,11 +29,11 @@
         margin: 1vh auto;
         height: 4vh;
         width: 10em;
-        color: #999;
+        color: #fff;
         font-size: 18px;
         line-height: 4vh;
         text-align: center;
-        border-bottom: 1px solid #999;
+        letter-spacing: 2px;
       }
       .pattern {
         text-align: center;
@@ -57,10 +57,12 @@
       }
     }
     .bottom {
+      position: relative;
       margin: 2vh 0.5vw;
       width: 19vw;
       height: 71vh;
       background-color: #666;
+      border-top-right-radius: 20px;
       .heatMapCon {
         display: flex;
         padding: 2vh 0;
@@ -209,6 +211,7 @@
         </div>
       </div>
       <div class="bottom">
+        <Load v-show="load2" :type="2"></Load>
         <div class="heatMapCon" v-show="pattern==0">
           <span
             v-for="(data, index) in heatMapRegion"
@@ -222,14 +225,14 @@
             <span class="set">已设服务点</span>
             <span class="rec">推荐服务点</span>
           </div>
-          <div class="choosePoint" v-show="!markerDate.data">请在地图上选择你要操作的服务点!</div>
-          <div class="dataCon" v-show="markerDate.data">
+          <div class="choosePoint" v-show="!serverId">请在地图上选择你要操作的服务点!</div>
+          <div class="dataCon" v-show="serverId">
             <div class="data">
               <span>
                 经度：
                 <input
                   type="text"
-                  :value="markerDate.longitude"
+                  :value="chooseMarker.data.longitude"
                   disabled
                   style="border:none;color:#f40"
                 />
@@ -238,44 +241,57 @@
                 纬度：
                 <input
                   type="text"
-                  :value="markerDate.latitude"
+                  :value="chooseMarker.data.latitude"
                   disabled
                   style="border:none;color:#f40"
                 />
               </span>
               <span>
                 数据1：
-                <input type="text" :value="markerDate.num1" />
+                <input type="text" v-model="markerDate.num1" />
               </span>
               <span>
                 数据2：
-                <input type="text" :value="markerDate.num2" />
+                <input type="text" v-model="markerDate.num2" />
               </span>
               <span>
                 负责人：
-                <input type="text" :value="markerDate.leader" />
+                <input type="text" v-model="markerDate.leader" />
               </span>
               <span>
                 志愿者数量：
-                <input type="text" :value="markerDate.volunteerNum" />
+                <input type="text" v-model="markerDate.volunteerNum" />
               </span>
             </div>
-            <div v-show="markerDate.type!='set'" class="add button">设为服务点</div>
-            <div v-show="markerDate.type=='set'" class="sure button" @click="changeMaeker">确定修改</div>
-            <div v-show="markerDate.type=='set'" class="delete button">删除服务点</div>
+            <div
+              v-show="chooseMarker.data.type!='set'"
+              class="add button"
+              @click="addMarker()"
+            >设为服务点</div>
+            <div
+              v-show="chooseMarker.data.type=='set'"
+              class="sure button"
+              @click="changeMaeker()"
+            >确定修改</div>
+            <div
+              v-show="chooseMarker.data.type=='set'"
+              class="delete button"
+              @click="deleteMarker()"
+            >删除服务点</div>
           </div>
         </div>
       </div>
     </div>
     <div class="right">
       <div id="map"></div>
+      <Load v-show="load1" :type="1"></Load>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Watch } from "vue-property-decorator";
-
+import Load from "@/components/loading/loading.vue";
 // import Vue from "vue";
 // import Component from "vue-class-component";
 import position1 from "../assets/position1.png";
@@ -286,14 +302,14 @@ import position3 from "../assets/position3.png";
 declare var AMap: any;
 declare var heatmapData: any;
 
-@Component
+@Component({ components: { Load } })
 export default class index extends Vue {
   // 地图实例
   map: any;
   // 模式
   pattern: number = 1; // 0--热力图， 1--服务点
   // 请求地址
-  reqUrl: string = "http://6e2dce46.ngrok.io";
+  reqUrl: string = "http://6033fd87.ngrok.io";
   // 热力图实例
   heatmap: any = null;
   // 显示热力图的所在区域编号
@@ -319,10 +335,23 @@ export default class index extends Vue {
   setMarkerList: any = [];
   // 推荐服务点实例
   recMarkerList: any = [];
-  // 鼠标选中的服务点实例
-  chooseMarker: any = null;
-  // 鼠标选中的服务点数据
-  markerDate: any = { data: null };
+  // 鼠标选中的服务点的圆圈标注
+  chooseMarkerCircle: any = null;
+  // 鼠标选中的服务点
+  chooseMarker: any = { data: {} };
+  // 鼠标选中的服务点id
+  serverId: any = null;
+  // 鼠标选中的服务点json数据
+  markerDate: any = {
+    num1: "",
+    num2: "",
+    leader: "",
+    volunteerNum: ""
+  };
+  // 地图加载
+  load1: any = false;
+  // 服务点数据加载
+  load2: any = false;
 
   // 初始化
   init(): void {
@@ -350,31 +379,45 @@ export default class index extends Vue {
   }
   // 热力图模块
   toHeatMap() {
+    if(this.load1) {
+      alert("操作频繁！")
+      return;
+    }
     this.pattern = 0;
     this.removeMarker();
     this.getHeatMap();
     this.map.setFitView();
-    if (this.chooseMarker) {
-      this.map.remove(this.chooseMarker);
+    if (this.chooseMarkerCircle) {
+      this.map.remove(this.chooseMarkerCircle);
     }
-    this.markerDate = { data: null };
   }
   // 服务点模块
   toService() {
+    if(this.load1) {
+      alert("操作频繁！")
+      return;
+    }
     this.pattern = 1;
+    this.serverId = null;
     this.deleteView();
     this.getMarker();
     this.hideHeatMap();
   }
   // 显示热力图
   getHeatMap() {
+    this.load1 = true;
     this.changeView(this.heatMapRegion[this.heatMapIndex]);
     console.log(this.heatMapIndex);
     this.$axios
       .get(this.reqUrl + "/api/data/listBlinds/" + this.heatMapIndex)
       .then((res: any) => {
         console.log(res.data.data);
-        console.log(this.heatmap);
+        this.load1 = false;
+        if (this.pattern == 1) {
+          this.heatmap.hide();
+          return;
+        }
+        this.heatmap.show();
         this.heatmap.setDataSet({ data: res.data.data });
       })
       .catch((err: any) => {
@@ -420,36 +463,40 @@ export default class index extends Vue {
   // 获取服务点数据
   getMarker() {
     // 获取推荐服务点
+    this.load1 = true;
     this.$axios
       .get(this.reqUrl + "/api/data/listLocation/0")
       .then((res: any) => {
-        console.log(res.data);
+        console.log('推荐服务点list',res.data);
         let recMarkerDatas = res.data.data;
         let len1 = recMarkerDatas.length;
         for (let i = 0; i < len1; i++) {
           this.setMarker(recMarkerDatas[i], "rec");
         }
-        setTimeout(() => {
-          this.map.setFitView();
-        }, 500);
+        // 获取设置的服务点
+        this.$axios
+          .get(this.reqUrl + "/api/data/serverPoints")
+          .then((res: any) => {
+            console.log('已设服务点list',res.data);
+            if (this.pattern == 0) {
+              return;
+            }
+            let setMarkerDatas = res.data.data;
+            let len2 = setMarkerDatas.length;
+            for (let i = 0; i < len2; i++) {
+              this.setMarker(setMarkerDatas[i], "set");
+            }
+            this.load1 = false;
+            this.map.setFitView();
+          })
+          .catch((err: any) => {
+            console.log(err);
+          });
       })
       .catch((err: any) => {
         console.log(err);
       });
-    // 获取设置的服务点
-    this.$axios
-      .get(this.reqUrl + "/api/data/serverPoints")
-      .then((res: any) => {
-        console.log(res.data);
-        let setMarkerDatas = res.data.data;
-        let len2 = setMarkerDatas.length;
-        for (let i = 0; i < len2; i++) {
-          this.setMarker(setMarkerDatas[i], "set");
-        }
-      })
-      .catch((err: any) => {
-        console.log(err);
-      });
+
     return;
     let recMarkerDatas = [
       {
@@ -515,21 +562,49 @@ export default class index extends Vue {
     }
     marker.on("click", this.clickMarker);
     this.map.add([marker]);
+    return marker;
   }
   // 给服务点绑定具体信息
   bindMarkerData(marker: any, obj: any, type: string) {
     marker.data = {
-      data: "666",
       serverId: obj.serverId,
       longitude: obj.longitude,
       latitude: obj.latitude,
       type: type
     };
+  }
+  // 获取服务点具体JSON数据
+  getMarkerData() {
+    if (!this.serverId) {
+      this.load2 = false;
+      return;
+    }
+    if (this.chooseMarker.data.type == "rec") {
+      this.markerDate = {
+        num1: "",
+        num2: "",
+        leader: "",
+        volunteerNum: ""
+      };
+      this.load2 = false;
+      return;
+    }
     this.$axios
-      .get(this.reqUrl + "/api/test/select/" + obj.serverId)
+      .get(this.reqUrl + "/api/test/select/" + this.serverId)
       .then((res: any) => {
-        console.log(JSON.parse(res.data.data));
-        Object.assign(marker.data, JSON.parse(res.data.data));
+        console.log('获取json',res.data);
+        // console.log(JSON.parse(res.data.data));
+        this.load2 = false;
+        if (res.data.data && res.data.data.data) {
+          this.markerDate = JSON.parse(res.data.data.data);
+        } else {
+          this.markerDate = {
+            num1: "",
+            num2: "",
+            leader: "",
+            volunteerNum: ""
+          };
+        }
       })
       .catch((err: any) => {
         console.log(err);
@@ -537,19 +612,28 @@ export default class index extends Vue {
   }
   // 服务点点击事件
   clickMarker(event: any) {
-    let marker = event.target;
-    this.markerDate = marker.data; // 显示服务点信息
-    if (this.chooseMarker) {
-      this.map.remove(this.chooseMarker);
+    if(this.load1 || this.load2) {
+      alert("操作频繁！")
+      return;
     }
-    this.chooseMarker = new AMap.CircleMarker({
+    let marker = event.target;
+    // this.markerDate = marker.data; // 显示服务点信息
+    // console.log(marker.data);
+    this.chooseMarker = marker;
+    this.serverId = marker.data.serverId;
+    this.load2 = true;
+    this.getMarkerData();
+    if (this.chooseMarkerCircle) {
+      this.map.remove(this.chooseMarkerCircle);
+    }
+    this.chooseMarkerCircle = new AMap.CircleMarker({
       center: new AMap.LngLat(marker.data.longitude, marker.data.latitude), // 圆心位置
       radius: 40, // 圆半径
       fillOpacity: 0,
       strokeColor: "#222", // 描边颜色
       strokeWeight: 5 // 描边宽度
     });
-    this.map.add(this.chooseMarker);
+    this.map.add(this.chooseMarkerCircle);
   }
   // 从地图移除全部服务点
   removeMarker() {
@@ -568,35 +652,50 @@ export default class index extends Vue {
     if (!judge) {
       return;
     }
+    this.load2 = true;
     let param = {
-      longitude: this.markerDate.lng,
-      latitude: this.markerDate.lat
+      longitude: this.chooseMarker.data.longitude,
+      latitude: this.chooseMarker.data.latitude
     };
-    // this.$axios.post(this.reqUrl+'/api/data/addServerpoint', param)
-    //   .then((res:any) => {
-    //     console.log(res.data)
-    //   })
-    //   .catch((err:any) => {
-    //     alert(err)
-    //   })
-    this.changeMaekerData(this.markerDate.serverId);
+    this.$axios
+      .post(this.reqUrl + "/api/data/addServerPoint", param)
+      .then((res: any) => {
+        console.log('添加',res.data);
+        // 在地图添加一个服务点
+        let obj = {
+          serverId: res.data.data.serverId,
+          longitude: this.chooseMarker.data.longitude,
+          latitude: this.chooseMarker.data.latitude
+        };
+        this.chooseMarker = this.setMarker(obj, "set");
+        this.serverId = obj.serverId;
+        // this.load2 = false;
+        this.changeMaekerData()
+      })
+      .catch((err: any) => {
+        console.log(err);
+      });
   }
   // 修改服务点json数据
-  changeMaekerData(serverId: any) {
+  changeMaekerData() {
     let param = {
       volunteerNum: this.markerDate.volunteerNum,
       leader: this.markerDate.leader,
       num1: this.markerDate.num1,
       num2: this.markerDate.num2
     };
-    this.$axios.post(this.reqUrl+'/api/test/update', {id: serverId, data: JSON.stringify(param)})
-      .then((res:any) => {
-        console.log(res.data)
+    this.$axios
+      .post(this.reqUrl + "/api/test/update", {
+        id: this.serverId,
+        data: JSON.stringify(param)
       })
-      .catch((err:any) => {
+      .then((res: any) => {
+        console.log('修改json',res.data);
+        this.getMarkerData();
+      })
+      .catch((err: any) => {
         console.log(err);
-      })
-    this.getMarker();
+      });
   }
   // 删除服务点
   deleteMarker() {
@@ -604,14 +703,21 @@ export default class index extends Vue {
     if (!judge) {
       return;
     }
-    // this.$axios.post(this.reqUrl+'/api/data/deleteServerpoint', {id: this.markerDate.serverId})
-    //   .then((res:any) => {
-    //     console.log(res.data)
-    //   })
-    //   .catch((err:any) => {
-    //     alert(err)
-    //   })
-    this.getMarker();
+    this.load2 = true;
+    this.$axios
+      .post(this.reqUrl + "/api/data/deleteServerPoint", {
+        serverId: this.serverId
+      })
+      .then((res: any) => {
+        console.log('删除',res.data);
+        this.serverId = null;
+        this.map.remove(this.chooseMarker);
+        this.map.remove(this.chooseMarkerCircle);
+        this.load2 = false;
+      })
+      .catch((err: any) => {
+        alert(err);
+      });
   }
   // 修改服务点
   changeMaeker() {
@@ -619,7 +725,8 @@ export default class index extends Vue {
     if (!judge) {
       return;
     }
-    this.changeMaekerData(this.markerDate.serverId);
+    this.load2 = true;
+    this.changeMaekerData();
   }
 
   mounted() {
